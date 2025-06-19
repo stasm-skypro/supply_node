e6bf81e3aec8fb6aaea826497c0f3f717b3a6993
Станислав Маяцкий <stasm226@gmail.com>
2025-06-19 18:45:19 +0500

    chore: add cors settings

--- config/settings.py
+++ config/settings.py
@@ -98,5 +98,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
 # Настройка CORS - домены, которым разрешён доступ
+CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOWED_ORIGINS = ["http://localhost:3000", "http://127.0.0.1:3000"]
 CSRF_TRUSTED_ORIGINS = ["http://localhost:3000", "http://127.0.0.1:3000"]
