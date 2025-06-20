49b5d68e2cb849c9b4e348b9541f0e3db0d1d830
Станислав Маяцкий <stasm226@gmail.com>
2025-06-20 09:42:28 +0500

    feat: add user to admin panel

--- user/admin.py
+++ user/admin.py
@@ -1,28 +1,23 @@
 from django.contrib import admin
 
-from supply.models import Node, Product
+from user.models import User
 
 
-@admin.register(Node)
-class NodeAdmin(admin.ModelAdmin):
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
     list_display = (
-        "name",
         "email",
         "phone",
-        "country",
-        "city",
-        "street",
-        "building_number",
-        "supplier",
-        "debt_to_supplier",
-        "created_at",
+        "first_name",
+        "last_name",
+        "image",
+        "role",
+        "is_active",
+        "is_staff",
+        "is_superuser",
     )
-    list_filter = ("city",)
-    search_fields = ("name", "email", "phone")
-
-
-@admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ("name", "model", "release_date", "owner")
-    list_filter = ("name",)
-    search_fields = ("name", "model")
+    list_filter = (
+        "role",
+        "email",
+    )
+    search_fields = ("email",)
